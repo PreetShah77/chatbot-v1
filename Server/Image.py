185a9fe7dbb58cb331067a873a9422c3bb21675f
PreetShah77 <98581633+PreetShah77@users.noreply.github.com>
2024-04-25 23:50:28 +0530

    Update Image.py

--- Server/Image.py
+++ Server/Image.py
@@ -48,4 +48,4 @@ def image_description():
     return jsonify({'description': response})
 
 if __name__ == '__main__':
-     app.run(host='0.0.0.0', port=5000 , debug=True)
+     app.run(debug=True)
